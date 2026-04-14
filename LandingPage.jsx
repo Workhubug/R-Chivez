58e849e891aaa6b53fc4f4f5f61041574438f964
Work Hub Sourcing <102583983+Workhubug@users.noreply.github.com>
2026-04-14 19:54:01 +0300

    Updated LandingPage.jsx to use an img tag for the r-chivez_image.png

---
+++ LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      {/* Replace spinning vinyl record SVG with an img tag */}
+      <img src="/r-chivez_image.png" alt="Chivez Record" />
+    </div>
+  );
+};
+
+export default LandingPage;
